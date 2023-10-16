b9329907d5614410e5e0343fe641fcf3f2e99d93
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-10-16 14:39:00 -0400

    Create UI 'catalogoCurso' & 'conexionDBR'

---
+++ CatalogoCursos/catalogoCursos/src/catalogocursos/vistaCatalogo.java
@@ -0,0 +1,232 @@
+package catalogocursos;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+
+public class vistaCatalogo extends javax.swing.JFrame {
+
+    public vistaCatalogo() {
+        initComponents();
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        baseCatalogo = new javax.swing.JPanel();
+        botonCurso1 = new javax.swing.JButton();
+        botonCurso2 = new javax.swing.JButton();
+        botonCurso3 = new javax.swing.JButton();
+        botonCurso4 = new javax.swing.JButton();
+        botonCurso5 = new javax.swing.JButton();
+        botonCurso6 = new javax.swing.JButton();
+        tituloCurso1 = new javax.swing.JLabel();
+        tituloCurso2 = new javax.swing.JLabel();
+        tituloCurso3 = new javax.swing.JLabel();
+        tituloCurso4 = new javax.swing.JLabel();
+        tituloCurso5 = new javax.swing.JLabel();
+        tituloCurso6 = new javax.swing.JLabel();
+        indicador1 = new javax.swing.JButton();
+        indicador2 = new javax.swing.JButton();
+        indicador3 = new javax.swing.JButton();
+        indicador4 = new javax.swing.JButton();
+        indicador5 = new javax.swing.JButton();
+        indicador6 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        baseCatalogo.setBackground(new java.awt.Color(48, 48, 48));
+
+        botonCurso1.setBackground(new java.awt.Color(217, 217, 217));
+
+        botonCurso2.setBackground(new java.awt.Color(217, 217, 217));
+
+        botonCurso3.setBackground(new java.awt.Color(217, 217, 217));
+
+        botonCurso4.setBackground(new java.awt.Color(217, 217, 217));
+
+        botonCurso5.setBackground(new java.awt.Color(217, 217, 217));
+
+        botonCurso6.setBackground(new java.awt.Color(217, 217, 217));
+
+        tituloCurso1.setBackground(new java.awt.Color(255, 255, 255));
+        tituloCurso1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        tituloCurso1.setForeground(new java.awt.Color(255, 255, 255));
+        tituloCurso1.setText("Curso 1");
+
+        tituloCurso2.setBackground(new java.awt.Color(255, 255, 255));
+        tituloCurso2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        tituloCurso2.setForeground(new java.awt.Color(255, 255, 255));
+        tituloCurso2.setText("Curso 2");
+
+        tituloCurso3.setBackground(new java.awt.Color(255, 255, 255));
+        tituloCurso3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        tituloCurso3.setForeground(new java.awt.Color(255, 255, 255));
+        tituloCurso3.setText("Curso 3");
+
+        tituloCurso4.setBackground(new java.awt.Color(255, 255, 255));
+        tituloCurso4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        tituloCurso4.setForeground(new java.awt.Color(255, 255, 255));
+        tituloCurso4.setText("Curso 4");
+
+        tituloCurso5.setBackground(new java.awt.Color(255, 255, 255));
+        tituloCurso5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        tituloCurso5.setForeground(new java.awt.Color(255, 255, 255));
+        tituloCurso5.setText("Curso 5");
+
+        tituloCurso6.setBackground(new java.awt.Color(255, 255, 255));
+        tituloCurso6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        tituloCurso6.setForeground(new java.awt.Color(255, 255, 255));
+        tituloCurso6.setText("Curso 6");
+
+        indicador1.setBackground(new java.awt.Color(250, 255, 0));
+
+        indicador2.setBackground(new java.awt.Color(250, 255, 0));
+
+        indicador3.setBackground(new java.awt.Color(250, 255, 0));
+
+        indicador4.setBackground(new java.awt.Color(250, 255, 0));
+
+        indicador5.setBackground(new java.awt.Color(250, 255, 0));
+
+        indicador6.setBackground(new java.awt.Color(250, 255, 0));
+
+        javax.swing.GroupLayout baseCatalogoLayout = new javax.swing.GroupLayout(baseCatalogo);
+        baseCatalogo.setLayout(baseCatalogoLayout);
+        baseCatalogoLayout.setHorizontalGroup(
+            baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, baseCatalogoLayout.createSequentialGroup()
+                .addContainerGap(104, Short.MAX_VALUE)
+                .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(baseCatalogoLayout.createSequentialGroup()
+                        .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(botonCurso4, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(baseCatalogoLayout.createSequentialGroup()
+                                .addComponent(tituloCurso4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(indicador4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(76, 76, 76)
+                        .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(botonCurso5, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(baseCatalogoLayout.createSequentialGroup()
+                                .addComponent(tituloCurso5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(indicador5, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(77, 77, 77)
+                        .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(baseCatalogoLayout.createSequentialGroup()
+                                .addComponent(tituloCurso6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(indicador6, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(botonCurso6, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(baseCatalogoLayout.createSequentialGroup()
+                        .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(botonCurso1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(baseCatalogoLayout.createSequentialGroup()
+                                .addComponent(tituloCurso1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(indicador1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(76, 76, 76)
+                        .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(botonCurso2, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(baseCatalogoLayout.createSequentialGroup()
+                                .addComponent(tituloCurso2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(indicador2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(77, 77, 77)
+                        .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, baseCatalogoLayout.createSequentialGroup()
+                                .addComponent(tituloCurso3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(indicador3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(botonCurso3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(93, 93, 93))
+        );
+        baseCatalogoLayout.setVerticalGroup(
+            baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(baseCatalogoLayout.createSequentialGroup()
+                .addGap(74, 74, 74)
+                .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(botonCurso1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(botonCurso2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(botonCurso3, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(tituloCurso1)
+                        .addComponent(tituloCurso2)
+                        .addComponent(tituloCurso3))
+                    .addComponent(indicador1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(indicador2, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(indicador3, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(74, 74, 74)
+                .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(botonCurso4, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(botonCurso5, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(botonCurso6, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(baseCatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(tituloCurso4)
+                        .addComponent(tituloCurso5)
+                        .addComponent(tituloCurso6))
+                    .addComponent(indicador4, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(indicador5, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(indicador6, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(90, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(baseCatalogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(baseCatalogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        //Ccambiamos el LookAndFell de la ventana
+        try {
+            UIManager.setLookAndFeel( new FlatDarkLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new vistaCatalogo().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel baseCatalogo;
+    private javax.swing.JButton botonCurso1;
+    private javax.swing.JButton botonCurso2;
+    private javax.swing.JButton botonCurso3;
+    private javax.swing.JButton botonCurso4;
+    private javax.swing.JButton botonCurso5;
+    private javax.swing.JButton botonCurso6;
+    private javax.swing.JButton indicador1;
+    private javax.swing.JButton indicador2;
+    private javax.swing.JButton indicador3;
+    private javax.swing.JButton indicador4;
+    private javax.swing.JButton indicador5;
+    private javax.swing.JButton indicador6;
+    private javax.swing.JLabel tituloCurso1;
+    private javax.swing.JLabel tituloCurso2;
+    private javax.swing.JLabel tituloCurso3;
+    private javax.swing.JLabel tituloCurso4;
+    private javax.swing.JLabel tituloCurso5;
+    private javax.swing.JLabel tituloCurso6;
+    // End of variables declaration//GEN-END:variables
+}
