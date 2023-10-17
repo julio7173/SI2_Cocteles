187f333d1fa18f67a95820f569a4315aa1714710
Rodrigo Quena <rodrigo.quena14@gmail.com>
2023-10-17 08:09:27 -0400

    Agregue proyecto cocteles

---
+++ Historia Rodrigo/Cocteles/src/cocteles/Interfas.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cocteles;
+
+/**
+ *
+ * @author Rodry
+ */
+public class Interfas {
+    String nombre;
+    String tiempo;
+    String popularidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(String tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getPopularidad() {
+        return popularidad;
+    }
+
+    public void setPopularidad(String popularidad) {
+        this.popularidad = popularidad;
+    }
+}
