3c79666a083d95de12b197e831e068268d2c36a8
Ailiez <dennis-galindo1@hotmail.com>
2023-10-17 22:47:01 -0400

    Se creó el proyecto catalogoLibros

---
+++ CatalogoLibros/catalogoLibros/src/main/java/catalogolibros/CatalogoLibros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package catalogolibros;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CatalogoLibros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
