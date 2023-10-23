8914f21d645ed9fee3b81e4caf5cc72577f5b6a3
Rodrigo Quena <rodrigo.quena14@gmail.com>
2023-10-23 12:59:25 -0400

    Avance de mi proyecto de notificaciones

---
+++ Notificaciones/JavaApplication3/src/notificaciones/ConeccionSql.java
@@ -0,0 +1,94 @@
+package notificaciones;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import java.sql.*;
+
+public class ConeccionSql {
+
+    public Connection conexion;
+    public Statement sentencia;
+    public ResultSet resultado;
+    public PreparedStatement ps;
+
+    public void ConectarBasedeDatos() {
+        try {
+            final String Controlador = "com.mysql.jdbc.Driver";
+            Class.forName(Controlador);
+            final String url_bd = "jdbc:mysql://localhost:3306/notificaciones";
+            conexion = DriverManager.getConnection(url_bd, "root", "");
+            sentencia = conexion.createStatement();
+            System.out.println("Coneccion realizada");
+        } catch (ClassNotFoundException | SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error ", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    public int existeBebida(String bebida) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = getConnection();
+
+        String sql = "SELECT count(nombre) FROM bebida WHERE nombre = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, bebida);
+
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+            return 1;
+        } catch (SQLException ex) {
+            return 1;
+        }
+    }
+    
+    public boolean registrar(ventana pro){
+        PreparedStatement ps = null;
+        Connection con = getConnection();
+        
+        String sql = "INSERT INTO bebida(nombre, popularidad) VALUES (?,?)";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1,pro.getNombre());
+            ps.setString(2,pro.getPopularidad());
+            ps.execute();
+            return true;
+        }catch(SQLException ex){
+            return false;
+        }
+    }
+    
+    public void DesconectarBasedeDatos() {
+        try {
+            if (conexion != null) {
+                if (sentencia != null) {
+                    sentencia.close();
+                }
+                conexion.close();
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            final String Controlador = "com.mysql.jdbc.Driver";
+            Class.forName(Controlador);
+            final String url_bd = "jdbc:mysql://localhost:3306/cocteles";
+            conexion = DriverManager.getConnection(url_bd, "root", "");
+            sentencia = conexion.createStatement();
+            System.out.println("Coneccion realizada");
+        } catch (ClassNotFoundException | SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error ", JOptionPane.ERROR_MESSAGE);
+        }
+        return conexion;
+    }
+}
